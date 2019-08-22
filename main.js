bfdea3d39f48391c0a8e9f36e64acd57a18c7255
Kasun Kulathunga <7507591+ThatDeaf-ITGuy@users.noreply.github.com>
2019-08-22 19:47:18 -0400

    Fixed some broken class references

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
                 var newToolbarItem = document.createElement('span');
                 newToolbarItem.classList.add('show-channels-button');
                 newToolbarItem.classList.add('active');
-                newToolbarItem.classList.add(document.querySelector('[class*="headerBar"] span[class^="iconMargin"]').classList[0])
+                newToolbarItem.classList.add(document.querySelector('[class*="toolbar"] span[class^="iconMargin"]').classList[0])
                 newToolbarItem.style.width = '26px'
                 newToolbarItem.style.position = 'relative'
                 newToolbarItem.style.top = '-2px'
@@ -15,7 +15,7 @@
                 newToolbarItem.style.cursor = 'pointer'
                 newToolbarItem.innerHTML = '<span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(5,1); width: 0; height: 5px; text-align: center;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(5.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(5,1); width: 0; height: 5px;">-</span><span style="display: block; color: #fefefe; -webkit-transform: scale(2.0, 1.0); -moz-transform:scale(2.0, 1.0); -ms-transform:scale(2.0, 1.0); -o-transform:scale(2.0, 1.0); transform: scale(5,1); width: 0; height: 5px;">-</span>';
 
-                document.querySelector('[class*="headerBar"]>:nth-child(2)').insertBefore(newToolbarItem,document.querySelector('[class^="divider"]'));
+                document.querySelector('[class*="toolbar"]>:nth-child(2)').insertBefore(newToolbarItem,document.querySelector('[class^="clickable"]'));
 
                 document.querySelector('.show-channels-button').addEventListener('click',function(e){
                     if(this.classList.contains('active')) {
